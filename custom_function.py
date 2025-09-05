6d6ded1aa4131d29452861ea7b03998272599405
Hien Do <thaihiendo190699@gmail.com>
2025-09-05 13:57:18 +0700

    Delete old files

---
+++ custom_function.py
@@ -0,0 +1,127 @@
+from sqlalchemy import create_engine,text
+import pandas as pd
+import streamlit as st
+from urllib.parse import quote_plus
+import os
+import datetime
+from datetime import datetime
+from datetime import date
+from pywinauto.application import Application
+
+
+
+# Function to open the Excel file
+def open_file_or_folder(file_path,is_file=True):
+    if is_file:
+        os.startfile(file_path)
+    else:
+        folder_path = os.path.dirname(file_path)
+        os.startfile(folder_path)
+        # Attach to the Explorer window
+        app = Application().connect(path='explorer.exe')
+    
+    #    Wait for the window to be opened and focus on it
+        app.top_window().set_focus()
+
+
+def get_update_time(file_path):
+    if os.path.exists(file_path):
+        last_modified_time = os.path.getmtime(file_path)
+        readable_update_time = datetime.fromtimestamp(last_modified_time).strftime('%Y-%m-%d %H:%M')
+        return readable_update_time 
+    return None
+
+
+
+@st.cache_data(show_spinner=False, ttl="20m")
+def get_reports(config_path, s_sheet_name, m_sheet_name, setting_sheet):
+    #Exclude folders
+    exclude_df = pd.read_excel(config_path, sheet_name=setting_sheet, usecols=['Excluded Folder'])
+    exclude_keywords = (
+        exclude_df["Excluded Folder"]
+        .dropna()                
+        .astype(str)             
+        .tolist()
+    )
+    #Structured reports
+    s_report_list = pd.read_excel(config_path, sheet_name=s_sheet_name)
+    results = []
+
+    for _, row in s_report_list.iterrows():
+        folder = row["Folder Path"]
+        pattern = row["Report Name Pattern"]
+
+        for root, dirs, files in os.walk(folder):
+            if any(kw.lower() in root.lower() for kw in exclude_keywords):
+                continue
+
+            for f in files:
+                if f.startswith("~$"):  
+                    continue
+                if pattern.lower() in f.lower():  
+                    file_path = os.path.join(root, f)
+                    rel_folder = os.path.dirname(os.path.relpath(file_path, folder)).replace("\\", " | ")
+                    results.append({
+                        "Folder Path": folder,
+                        "Report Name Pattern": pattern,
+                        "Report Name": f,
+                        "file_link": file_path,
+                        "Extra Description": rel_folder,
+                        "Update Time Raw": get_update_time(file_path)
+                    })
+    s_df = pd.DataFrame(results).merge(
+        s_report_list, 
+        on=["Folder Path", "Report Name Pattern"], 
+        how='inner')
+    #Mix reports
+    m_report_list = pd.read_excel(config_path, sheet_name=m_sheet_name)
+    m_report_list['Update Time Raw'] = m_report_list['file_link'].apply(get_update_time)
+    #Append
+    report_list = pd.concat([s_df, m_report_list], ignore_index=True)
+    #Update Warning
+    threshold_df = pd.read_excel(config_path, sheet_name=setting_sheet, usecols=['Update Frequency', 'Threshold'])
+    report_list = get_update_warning(report_list, threshold_df)
+    return report_list
+    
+
+def get_update_warning(report_list, threshold):  
+    report_list = report_list.merge(threshold, how='left', on='Update Frequency')
+    # chuyển sang datetime, lỗi -> NaT
+    report_list["Update Time Val"] = pd.to_datetime(report_list["Update Time Raw"], errors='coerce')
+    
+    # tính số ngày kể từ Update Time
+    days_since_update = (pd.Timestamp('today') - report_list["Update Time Val"]).dt.days
+    
+    # cột Has Update Warning: NaT hoặc vượt Threshold
+    report_list["Has Update Warning"] = (
+        days_since_update.isna() | (days_since_update > report_list["Threshold"])
+    ).map({True: "Yes", False: "No"})
+    
+    # cột Update Warning
+    def format_warning(row):
+        if pd.isna(row["Update Time Val"]):
+            return "⚠️"
+        text = row["Update Time Raw"]
+        if (pd.Timestamp('today') - row["Update Time Val"]).days > row["Threshold"]:
+            text += " ⚠️"
+        return text
+    
+    report_list["Update Time"] = report_list.apply(format_warning, axis=1)
+    
+    return report_list
+
+# def get_filter_options(df, columns, sep=","):
+#     filter_options = {}
+#     for col in columns:
+#         if col not in df.columns:
+#             return []
+#         filter_options[col] = sorted(df[col].dropna().astype(str).str.split(sep).explode().str.strip().unique().tolist())
+#     return filter_options
+
+@st.cache_data(show_spinner=False, ttl="20m")
+def get_unique_elements(series):
+    elements = set()
+    for item in series.dropna():
+        parts = [x.strip() for x in str(item).split(',')]
+        elements.update(parts)
+    return sorted(elements)
---
+++ main.py
@@ -0,0 +1,189 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import os
+import re
+from custom_function import  get_reports, open_file_or_folder, get_unique_elements
+from pywinauto import Application
+from st_aggrid import AgGrid, GridOptionsBuilder, GridUpdateMode
+from datetime import datetime, timedelta
+from streamlit_cookies_manager import EncryptedCookieManager
+import json
+import warnings
+warnings.filterwarnings("ignore")
+
+# Config
+config_path = r'D:\Report_dictionary\Report_Dictionary.xlsx'
+mixed_sheet = 'Mixed_Reports'
+structured_sheet = 'Structured_Reports'
+setting_sheet = 'Settings'
+
+# Khởi tạo cookie manager
+cookies = EncryptedCookieManager(
+    prefix="myapp/",
+    password="ngnhb",
+)
+
+if not cookies.ready():
+    st.stop()
+
+
+# Load cookie lần đầu
+cookie_key = "filters"
+
+if "filters_loaded" not in st.session_state:
+    if cookie_key in cookies:
+        saved_filter = json.loads(cookies[cookie_key])
+        st.session_state["users_filter"] = saved_filter.get("users_filter", [])
+    st.session_state["filters_loaded"] = True  # Đánh dấu đã load cookie
+
+
+# Layout
+st.set_page_config(layout="wide")
+
+st.markdown("""
+    <style>
+        .block-container {
+            padding-top: 1.5rem;
+        }
+    
+}
+    </style>
+""", unsafe_allow_html=True)
+st.header("Report Dictionary", divider=True)
+
+
+#Load Report List
+with st.spinner("Loading..."):
+    report_list = get_reports(config_path=config_path, s_sheet_name=structured_sheet, m_sheet_name=mixed_sheet, setting_sheet=setting_sheet)
+
+
+# Filter
+col1, col2, col3, col4, col5, col6 = st.columns([2, 2, 2, 2, 2, 2])
+with col1:
+    search_tags = st.text_input("🔎", placeholder="Search...")
+with col2:
+    cate_filter = st.multiselect("Category", options=get_unique_elements(report_list["Category"]))
+with col3:
+    object_filter = st.multiselect("Focused Objects", options=get_unique_elements(report_list["Focused Objects"]))
+with col4:
+    users_filter = st.multiselect("Intended Users", 
+                                  options=get_unique_elements(report_list["Intended Users"]), 
+                                  default=st.session_state.get("users_filter", []),
+                                  key="users_filter")
+with col5:
+    update_filter = st.multiselect("Has Update Warning", options=['Yes', 'No'])
+with col6:
+    type_filter = st.multiselect("Type", options=get_unique_elements(report_list["Type"]), default="Report")
+
+st.markdown("<div style='height: 30px;'></div>", unsafe_allow_html=True)
+
+
+
+# Chỉ save cookie khi có thay đổi thật sự
+if json.loads(cookies[cookie_key]).get("users_filter", []) != users_filter:
+    cookies[cookie_key] = json.dumps({"users_filter": st.session_state["users_filter"]})
+    cookies.save()
+    
+# Filter Report List
+filtered_report_list = report_list
+
+if search_tags:
+    pattern = r"\b" + re.escape(search_tags.lower()) + r"\b"
+    filtered_report_list = filtered_report_list[
+        filtered_report_list[['Report Name', 'Purpose', 'Extra Description']]
+        .apply(lambda row: bool(re.search(pattern, ' '.join(row.astype(str)).lower())), axis=1)
+    ]
+if cate_filter:
+    cate_pattern = '|'.join(cate_filter)
+    filtered_report_list = filtered_report_list[(filtered_report_list["Category"].str.contains(cate_pattern, case=False, na=False)) | (filtered_report_list["Category"].str.upper() == "ALL")]
+if object_filter:
+    object_pattern = '|'.join(object_filter)
+    filtered_report_list = filtered_report_list[filtered_report_list["Focused Objects"].str.contains(object_pattern, case=False, na=False)]
+if users_filter:
+    users_pattern = '|'.join(users_filter)
+    filtered_report_list = filtered_report_list[filtered_report_list["Intended Users"].str.contains(users_pattern, case=False, na=False) | (filtered_report_list["Category"].str.upper() == "ALL")]
+if update_filter:
+    update_pattern = '|'.join(update_filter)
+    filtered_report_list = filtered_report_list[filtered_report_list["Has Update Warning"].str.contains(update_pattern, case=False, na=False)]
+if type_filter:
+    type_pattern = '|'.join(type_filter)
+    filtered_report_list = filtered_report_list[filtered_report_list["Type"].str.contains(type_pattern, case=False, na=False)]
+if filtered_report_list.empty:
+    st.warning("⚠️ No matching reports found. Please adjust your filters.")
+else:
+
+    filtered_report_list = filtered_report_list[['Report Name', 'Purpose', 'Category', 'Focused Objects', 'Time Dimension', 'Update Frequency', 'Update Time', 'PIC', 'Extra Description', 'file_link']]
+    gb = GridOptionsBuilder.from_dataframe(filtered_report_list)
+    gb.configure_selection(selection_mode="single", use_checkbox=False)  # Không checkbox
+    
+    gb.configure_column("file_link", hide=True)  # ẨN cột Path
+    # Wrap text cho các cột dài
+
+    for col in filtered_report_list.columns:
+        gb.configure_column(
+            col,
+            headerClass="bold-header",
+            cellStyle={
+                "display": "flex",
+                "alignItems": "center",        
+                "justifyContent": "flex-start", 
+                "paddingLeft": "5px",
+                "paddingRight": "5px", 
+                "paddingTop": "5px",
+                "paddingBottom": "5px",       
+                "lineHeight": "1.2", 
+                "whiteSpace": "normal"           
+            },
+            maxWidth=250,
+            suppressSizeToFit=True,
+            autoHeight=True
+        )
+
+
+    gb.configure_column("Report Name", pinned="left")
+    grid_options = gb.build()
+
+    # Tạo chỗ trống để update info sau
+    info_placeholder = st.empty()   
+    custom_css = {
+    ".ag-header-cell": {
+        "background-color": "#0d96fd !important",
+        "color": "white !important",
+        "font-weight": "bold !important"
+    }
+    }
+
+    grid_response = AgGrid(filtered_report_list, gridOptions=grid_options, update_mode=GridUpdateMode.SELECTION_CHANGED, theme="balham", custom_css=custom_css)
+
+
+    selected = grid_response["selected_rows"]
+
+# Select and Open Report
+    if  selected is not None and len(selected) > 0:
+        
+        with info_placeholder.container():
+            file_path = selected.iloc[0]['file_link']
+            
+            col1, col2, col3, _ = st.columns([3,1,1, 4], gap="small")
+            with col1:
+                st.write(f"Selected report: **{selected.iloc[0]['Report Name']}**")
+            with col2:
+                if st.button('Open File  ', key=f'file_{0}'):
+                    open_file_or_folder(file_path)
+            with col3:
+                if st.button('Open Folder', key=f'folder_{0}'):
+                    open_file_or_folder(file_path, False)
+                    
+    else:
+        info_placeholder.info("Select a report row to open file/folder.")
+
+
+
+# Hiển thị số lượng báo cáo
+    num_reports = filtered_report_list.shape[0]
+    st.markdown(f"**Showing {num_reports} report(s).**")
+
+
+
+
